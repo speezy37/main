722e6905081dab14a22bfb300a273f3b65efbbbf
Nur Hafizuddin Kamaruddin <>
2018-10-08 22:45:54 +0800

    Travis fix

--- src/test/java/seedu/address/logic/commands/AddCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddCommandTest.java
@@ -189,11 +189,7 @@ public class AddCommandTest {
         public void commitLeaveList() {
             throw new AssertionError("This method should not be called.");
         }
-
-        @Override
-        public boolean hasLogin() {
-            throw new AssertionError("This method should not be called.");
-        }
+        
     }
 
     /**
