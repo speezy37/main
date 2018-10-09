30b7990b529d9869cc6fd85d42d2fc813516b431
Chong Yu <chuayu37@gmail.com>
2018-10-09 21:51:36 +0800

    Schedule Command Parser

--- src/main/java/seedu/address/logic/commands/ScheduleCommand.java
+++ src/main/java/seedu/address/logic/commands/ScheduleCommand.java
@@ -2,15 +2,9 @@ package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-
-import javafx.collections.ObservableList;
+import seedu.address.commons.core.index.Index;
 import seedu.address.logic.CommandHistory;
 import seedu.address.model.Model;
-import seedu.address.model.person.Person;
-import seedu.address.model.person.UniquePersonList;
 
 /**
  * List schedule of a person in the address book to the user.
@@ -25,33 +19,21 @@ public class ScheduleCommand extends Command {
     public static final String MESSAGE_SCHEDULE_SUCCESS = "Listed Schedule:";
     public static final String MESSAGE_SCHEDULE_FAIL = "Person not found in address book.";
 
-    private static final String FILE_PATH = "data\\schedule.txt";
+    private final Index index;
+
+    public ScheduleCommand(Index index){
+        requireNonNull(index);
+        this.index = index;
+    }
 
     @Override
     public CommandResult execute(Model model, CommandHistory history) {
         requireNonNull(model);
         requireNonNull(history);
 
-        StringBuilder builder = new StringBuilder();
-        for(int i=0; i<model.getAddressBook().getPersonList().size(); i++){
-            builder.append(i+1)
-                    .append(": ")
-                    .append(model.getAddressBook().getPersonList().get(i).getSchedule().value)
-                    .append("\n");
-        }
-        return new CommandResult(MESSAGE_SCHEDULE_SUCCESS + "\n" +
-                builder.toString());
-        /*
-        File file = new File(FILE_PATH);
+        String schedule = model.getAddressBook().getPersonList().get(index.getZeroBased())
+                .getSchedule().value;
 
-        try {
-            BufferedReader br = new BufferedReader(new FileReader(file));
-
-            String st = br.readLine();
-            return new CommandResult(MESSAGE_SCHEDULE_SUCCESS + "\n" + st);
-        } catch (Exception e) {
-            return new CommandResult(MESSAGE_SCHEDULE_FAIL);
-        }
-        */
+        return new CommandResult(MESSAGE_SCHEDULE_SUCCESS + "\n" + schedule);
     }
 }
