e5411f57fe407a52cad05b4b6f9c6dfea63d8f19
Woonhian <limwoonhian@u.nus.edu>
2018-10-10 00:59:44 +0800

    Model: Updated Model. Logic: Updated FilterDepartmentCommand

--- src/main/java/seedu/address/logic/commands/FilterDepartmentCommand.java
+++ src/main/java/seedu/address/logic/commands/FilterDepartmentCommand.java
@@ -32,9 +32,9 @@ public class FilterDepartmentCommand extends Command {
     @Override
     public CommandResult execute(Model model, CommandHistory history) {
         requireNonNull(model);
-        model.updateFilteredDepartmentList(predicate);
+        model.updateFilteredPersonList(predicate);
         return new CommandResult(
-                String.format(Messages.MESSAGE_PERSONS_LISTED_OVERVIEW, model.getFilteredDepartmentList().size()));
+                String.format(Messages.MESSAGE_PERSONS_LISTED_OVERVIEW, model.getFilteredPersonList().size()));
     }
 
     @Override
--- src/main/java/seedu/address/model/Model.java
+++ src/main/java/seedu/address/model/Model.java
@@ -50,20 +50,11 @@ public interface Model {
     /** Returns an unmodifiable view of the filtered person list */
     ObservableList<Person> getFilteredPersonList();
 
-    /** Returns an unmodifiable view of the filtered department list */
-    ObservableList<Person> getFilteredDepartmentList();
-
     /**
      * Updates the filter of the filtered person list to filter by the given {@code predicate}.
      * @throws NullPointerException if {@code predicate} is null.
      */
     void updateFilteredPersonList(Predicate<Person> predicate);
-
-    /**
-     * Updates the filter of the filtered department list to filter by the given {@code predicate}.
-     * @throws NullPointerException if {@code predicate} is null.
-     */
-    void updateFilteredDepartmentList(Predicate<Person> predicate);
 
     /**
      * Returns true if the model has previous address book states to restore.
