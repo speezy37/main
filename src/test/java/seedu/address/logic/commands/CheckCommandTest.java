09960eb0049f856d58bb38e6fceab8edeb4a1382
pinjuen <limpinjuen@gmail.com>
2018-10-12 16:16:17 +0800

    CheckCommand update 1

--- src/test/java/seedu/address/logic/commands/CheckCommandTest.java
+++ src/test/java/seedu/address/logic/commands/CheckCommandTest.java
@@ -1,6 +1,15 @@
 package seedu.address.logic.commands;
 
-import static seedu.address.logic.commands.CheckCommand.MESSAGE_NOT_IMPLEMENTED_YET;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static seedu.address.logic.commands.CheckCommand.MESSAGE_ARGUMENTS;
+
+import static seedu.address.logic.commands.CommandTestUtil.VALID_MODE_AMY;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_MODE_BOB;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_NRIC_AMY;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_NRIC_BOB;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_PASSWORD_AMY;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_PASSWORD_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
@@ -19,6 +28,36 @@ public class CheckCommandTest {
 
     @Test
     public void execute() {
-        assertCommandFailure(new CheckCommand(), model, new CommandHistory(), MESSAGE_NOT_IMPLEMENTED_YET);
+        final String mode = "in";
+
+        assertCommandFailure(new CheckCommand(VALID_NRIC_AMY, VALID_PASSWORD_AMY, mode), model, new CommandHistory(),
+                String.format(MESSAGE_ARGUMENTS, VALID_NRIC_AMY, VALID_PASSWORD_AMY, mode));
+    }
+
+    @Test
+    public void equals() {
+        final CheckCommand standardCommand = new CheckCommand(VALID_NRIC_AMY, VALID_PASSWORD_AMY, VALID_MODE_AMY);
+
+        // same values -> returns true
+        CheckCommand commandWithSameValues = new CheckCommand(VALID_NRIC_AMY, VALID_PASSWORD_AMY, VALID_MODE_AMY);
+        assertTrue(standardCommand.equals(commandWithSameValues));
+
+        // same object -> returns true
+        assertTrue(standardCommand.equals(standardCommand));
+
+        // null -> returns false
+        assertFalse(standardCommand.equals(null));
+
+        // different types -> returns false
+        assertFalse(standardCommand.equals(new ClearCommand()));
+
+        // different nric -> returns false
+        assertFalse(standardCommand.equals(new CheckCommand(VALID_NRIC_BOB, VALID_PASSWORD_AMY, VALID_MODE_AMY)));
+
+        // different password -> returns false
+        assertFalse(standardCommand.equals(new CheckCommand(VALID_NRIC_AMY, VALID_PASSWORD_BOB, VALID_MODE_AMY)));
+
+        // different mode -> returns false
+        assertFalse(standardCommand.equals(new CheckCommand(VALID_NRIC_AMY, VALID_PASSWORD_AMY, VALID_MODE_BOB)));
     }
 }
