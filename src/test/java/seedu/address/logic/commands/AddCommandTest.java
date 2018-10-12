b29de423e323715e752ec81b782fffa768627030
Woonhian <limwoonhian@u.nus.edu>
2018-10-12 21:07:22 +0800

    Logic: Updated AddCommandTest

--- src/test/java/seedu/address/logic/commands/AddCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddCommandTest.java
@@ -136,6 +136,11 @@ public class AddCommandTest {
         }
 
         @Override
+        public void sortEmployee(String field, String order) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
         public void updatePerson(Person target, Person editedPerson) {
             throw new AssertionError("This method should not be called.");
         }
