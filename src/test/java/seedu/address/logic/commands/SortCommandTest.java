4d367f76a9dfb65c970ba7efb09f6fa1a9239f4c
Woonhian <limwoonhian@u.nus.edu>
2018-10-13 15:03:57 +0800

    Updated SortCommandTest

--- src/test/java/seedu/address/logic/commands/SortCommandTest.java
+++ src/test/java/seedu/address/logic/commands/SortCommandTest.java
@@ -13,7 +13,6 @@ import org.junit.rules.ExpectedException;
 
 import seedu.address.commons.core.Messages;
 import seedu.address.logic.CommandHistory;
-import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
