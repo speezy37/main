b1d7c0a89b3398fe13ca46ca03e8b805e3b93c5d
pinjuen <limpinjuen@gmail.com>
2018-10-15 20:45:12 +0800

    appveyor & travis fix

--- src/test/java/guitests/guihandles/PersonCardHandle.java
+++ src/test/java/guitests/guihandles/PersonCardHandle.java
@@ -2,13 +2,14 @@ package guitests.guihandles;
 
 import java.util.List;
 import java.util.stream.Collectors;
+
+import com.google.common.collect.ImmutableMultiset;
+
 import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.layout.Region;
 
 import seedu.address.model.person.Person;
-
-import com.google.common.collect.ImmutableMultiset;
 
 /**
  * Provides a handle to a person card in the person list panel.
