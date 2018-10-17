1194c7968a97863dc83fa2e14045a149e6f39513
Chong Yu <chuayu37@gmail.com>
2018-10-18 00:39:11 +0800

    Switch branch

--- src/test/java/seedu/address/logic/commands/CommandTestUtil.java
+++ src/test/java/seedu/address/logic/commands/CommandTestUtil.java
@@ -58,6 +58,13 @@ public class CommandTestUtil {
     public static final String VALID_MODE_AMY = "in";
     public static final String VALID_MODE_BOB = "out";
 
+    public static final String VALID_TIME_START_AMY = "1000";
+    public static final String VALID_TIME_START_BOB = "1100";
+    public static final String VALID_TIME_END_AMY = "Amy Bee";
+    public static final String VALID_TIME_END_BOB = "Amy Bee";
+    public static final String VALID_VENUE_AMY = "Amy Bee";
+    public static final String VALID_VENUE_BOB = "Amy Bee";
+
     public static final String NAME_DESC_AMY = " " + PREFIX_NAME + VALID_NAME_AMY;
     public static final String NAME_DESC_BOB = " " + PREFIX_NAME + VALID_NAME_BOB;
     public static final String NRIC_DESC_AMY = " " + PREFIX_NRIC + VALID_NRIC_AMY;
--- src/test/java/seedu/address/logic/parser/SetScheduleCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/SetScheduleCommandParserTest.java
@@ -1,12 +1,15 @@
 package seedu.address.logic.parser;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_NAME_AMY;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TIME_END;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TIME_START;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_VENUE;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 
 import org.junit.Test;
 
+import seedu.address.logic.commands.EditCommand;
 import seedu.address.logic.commands.SetScheduleCommand;
 
 public class SetScheduleCommandParserTest {
@@ -21,7 +24,14 @@ public class SetScheduleCommandParserTest {
 
     @Test
     public void parse_missingParts_failure() {
+        // no index specified
+        assertParseFailure(parser, VALID_NAME_AMY, MESSAGE_INVALID_FORMAT);
 
+        // no field specified
+        assertParseFailure(parser, "1", SetScheduleCommand.MESSAGE_SCHEDULE_FAIL);
+
+        // no index and no field specified
+        assertParseFailure(parser, "", MESSAGE_INVALID_FORMAT);
     }
 
     @Test
