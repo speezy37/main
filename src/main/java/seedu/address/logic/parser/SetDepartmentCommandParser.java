875da637e3f3e14970d296638b8a43dec434e989
Woonhian <e0176863@u.nus.edu>
2018-10-27 01:27:50 +0800

    Fix Travis

--- src/main/java/seedu/address/logic/parser/SetDepartmentCommandParser.java
+++ src/main/java/seedu/address/logic/parser/SetDepartmentCommandParser.java
@@ -8,7 +8,6 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_DEPARTMENT;
 import java.util.stream.Stream;
 
 import seedu.address.commons.core.index.Index;
-import seedu.address.logic.commands.EditCommand;
 import seedu.address.logic.commands.SetDepartmentCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.person.Department;
