ad482ac32cfa7c38cd86e77d9a769a114117465b
Woonhian <e0176863@u.nus.edu>
2018-10-27 01:04:32 +0800

    Added SetDepartmentCommandParserTest

---
+++ src/test/java/seedu/address/logic/parser/SetDepartmentCommandParserTest.java
@@ -0,0 +1,62 @@
+package seedu.address.logic.parser;
+
+import org.junit.Test;
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.SetDepartmentCommand;
+import seedu.address.model.person.Department;
+import seedu.address.testutil.TypicalIndexes;
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+
+public class SetDepartmentCommandParserTest {
+    private SetDepartmentCommandParser parser = new SetDepartmentCommandParser();
+
+    @Test
+    public void parse_missingParts_failure() {
+        String setDepartmentCommand = SetDepartmentCommand.COMMAND_WORD + " %s %s";
+        // no index specified
+        assertParseFailure(parser, String.format(setDepartmentCommand, null, "Junior Management"),
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, SetDepartmentCommand.MESSAGE_USAGE));
+
+        // no field specified
+        assertParseFailure(parser, String.format(setDepartmentCommand, "1", null),
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, SetDepartmentCommand.MESSAGE_USAGE));
+
+        // no index and no field specified
+        assertParseFailure(parser, String.format(setDepartmentCommand, null, null),
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, SetDepartmentCommand.MESSAGE_USAGE));
+    }
+
+    @Test
+    public void parse_invalidIndex_failure() {
+        String setDepartmentCommand = "invalid" + CliSyntax.PREFIX_DEPARTMENT
+                + "Junior Management";
+        assertParseFailure(parser, setDepartmentCommand,
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, SetDepartmentCommand.MESSAGE_USAGE));
+    }
+
+    @Test
+    public void parse_invalidDepartment_failure() {
+        String setDepartmentCommand = "1 " + CliSyntax.PREFIX_DEPARTMENT + "Jun1or Management";
+        assertParseFailure(parser, setDepartmentCommand,
+                Department.MESSAGE_DEPARTMENT_CONSTRAINTS);
+
+        setDepartmentCommand = "1 " + CliSyntax.PREFIX_DEPARTMENT + "Junior Manage";
+        assertParseFailure(parser, setDepartmentCommand,
+                Department.MESSAGE_DEPARTMENT_CONSTRAINTS);
+
+    }
+
+    @Test
+    public void parse_validParameters_success() {
+        Index targetIndex = TypicalIndexes.INDEX_FIRST_PERSON;
+        Department expectedDepartment = new Department("Junior Management");
+        SetDepartmentCommand expectedCommandReturn = new SetDepartmentCommand(targetIndex, expectedDepartment);
+
+        String sdl = String.valueOf(targetIndex.getOneBased()) + " " + CliSyntax.PREFIX_DEPARTMENT
+                + expectedDepartment.fullDepartment;
+        assertParseSuccess(parser, sdl, expectedCommandReturn);
+    }
+}
