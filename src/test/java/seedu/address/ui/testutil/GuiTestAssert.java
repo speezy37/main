af06c530201972c50fd235a015ca3d7ed96170eb
pinjuen <limpinjuen@gmail.com>
2018-11-01 02:45:21 +0800

    update on test

--- src/test/java/seedu/address/ui/testutil/GuiTestAssert.java
+++ src/test/java/seedu/address/ui/testutil/GuiTestAssert.java
@@ -35,9 +35,9 @@ public class GuiTestAssert {
         assertEquals(expectedPerson.getEmail().value, actualCard.getEmail());
         assertEquals(expectedPerson.getDepartment().fullDepartment, actualCard.getDepartment());
         assertEquals(expectedPerson.getAddress().value, actualCard.getAddress());
-        assertEquals(expectedPerson.getMode().value, actualCard.getMode());
-        assertEquals(expectedPerson.getWorkingRate().value, actualCard.getWorkingRate());
-        assertEquals(expectedPerson.getCheckedInTime().value, actualCard.getCheckedInTime());
+        assertEquals("Checked " + expectedPerson.getMode().value, actualCard.getMode());
+        assertEquals("$" + expectedPerson.getWorkingRate().value + " per hour", actualCard.getWorkingRate());
+        assertEquals("at " + expectedPerson.getCheckedInTime().value + "\n\n", actualCard.getCheckedInTime());
         assertEquals(expectedPerson.getTags().stream().map(tag -> tag.tagName).collect(Collectors.toList()),
                 actualCard.getTags());
     }
