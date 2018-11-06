64105cf8ba1cd1f045f3a9ea0df66bd966a24f9c
Hafizuddin-NUS <e0176876@u.nus.edu>
2018-11-06 15:29:08 +0800

    Clear unused imports

--- src/main/java/seedu/address/logic/commands/EditLeaveCommand.java
+++ src/main/java/seedu/address/logic/commands/EditLeaveCommand.java
@@ -8,7 +8,6 @@ import java.util.Optional;
 
 import seedu.address.commons.core.Messages;
 import seedu.address.commons.core.index.Index;
-import seedu.address.commons.util.CollectionUtil;
 import seedu.address.logic.CommandHistory;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
